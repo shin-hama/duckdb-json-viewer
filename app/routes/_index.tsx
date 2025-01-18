0adfd6df43432d807439b0ea96e157d3aa7b801f
shin-hama <htysivista@gmail.com>
2025-01-18 15:34:35 +0900

    FEAT: 基本的なレイアウトを実装

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,6 +5,7 @@ import { getDb } from '@/infrastructures/db';
 import { DataTable } from '@/components/DataTable';
 import SqlEditor from '@/components/SqlEditor.client';
 import { ClientOnly } from 'remix-utils/client-only';
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
@@ -30,33 +31,35 @@ export default function Index() {
   };
 
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Welcome to <span className="sr-only">Remix</span>
-          </h1>
-          <div className="h-[144px] w-[434px]">
-            <img src="/logo-light.png" alt="Remix" className="block w-full dark:hidden" />
-            <img src="/logo-dark.png" alt="Remix" className="hidden w-full dark:block" />
+    <div className="h-screen">
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel minSize={15} maxSize={40} defaultSize={20}>
+          <div>
+            <p>Sidebar</p>
           </div>
-        </header>
-        <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-          <Button variant="default" onClick={test}>
-            test
-          </Button>
-
-          <div className="w-80">
-            <ClientOnly>{() => <SqlEditor />}</ClientOnly>
-          </div>
-          {data.length > 0 && (
-            <DataTable
-              columns={Object.keys(data[0] ?? {}).map((key) => ({ header: key, accessorKey: key }))}
-              data={data}
-            />
-          )}
-        </nav>
-      </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel>
+              <ClientOnly>{() => <SqlEditor />}</ClientOnly>
+              <Button variant="default" onClick={test}>
+                test
+              </Button>
+            </ResizablePanel>
+            <ResizableHandle withHandle />
+            <ResizablePanel>
+              <DataTable
+                columns={Object.keys(data[0] ?? {}).map((key) => ({
+                  header: key,
+                  accessorKey: key,
+                }))}
+                data={data}
+              />
+            </ResizablePanel>
+          </ResizablePanelGroup>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
